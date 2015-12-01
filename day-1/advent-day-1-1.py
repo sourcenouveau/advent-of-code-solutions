938a8bb784975cb0b27eb6385079f389da9cc03e
M. Dudley <emddudley@gmail.com>
2015-12-01 13:12:49 -0500

    Added Day 1 solutions

---
+++ day-1/advent-day-1-1.py
@@ -0,0 +1,6 @@
+# Advent of Code Solutions: Day 1, part 1
+# http://github.com/emddudley/advent-of-code-solutions
+
+with open('input', 'r') as input:
+    instructions = input.read()
+    print(instructions.count('(') - instructions.count(')'))
---
+++ day-1/advent-day-1-2.py
@@ -0,0 +1,26 @@
+# Advent of Code Solutions: Day 1, part 2
+# http://github.com/emddudley/advent-of-code-solutions
+
+with open('input', 'r') as input:
+    instructions = input.read()
+
+floor = 0
+position = 0
+underground = False
+
+for instruction in instructions:
+    position += 1
+
+    if instruction == '(':
+        floor += 1
+    elif instruction == ')':
+        floor -= 1
+
+    if floor < 0:
+        underground = True
+        break
+
+if underground:
+    print(position)
+else:
+    print("Error: Santa never went underground.")
