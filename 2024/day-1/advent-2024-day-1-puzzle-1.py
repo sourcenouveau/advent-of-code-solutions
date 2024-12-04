211752561ed192a45b1574c4d2190fc083f22b15
M. Dudley <emddudley@gmail.com>
2024-12-04 10:02:56 -0500

    Solutions for 2024 Day 1

---
+++ 2024/day-1/advent-2024-day-1-puzzle-1.py
@@ -0,0 +1,16 @@
+"""
+Day 1: Historian Hysteria --- Part One
+https://adventofcode.com/2024/day/1
+"""
+
+def main():
+    with open("input") as f:
+        left, right = zip(*(map(int, line.strip().split("   ")) for line in f))
+
+    distance = sum(abs(l - r) for l, r in zip(sorted(left), sorted(right)))
+
+    print(distance)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/day-1/advent-2024-day-1-puzzle-2.py
@@ -0,0 +1,24 @@
+"""
+Day 1: Historian Hysteria --- Part Two
+https://adventofcode.com/2024/day/1
+"""
+
+def main():
+    with open("input") as f:
+        left, right = zip(*(map(int, line.strip().split("   ")) for line in f))
+
+    counts = {}
+
+    for r in right:
+        counts[r] = counts.get(r, 0) + 1
+
+    similarity = 0
+
+    for l in left:
+        similarity += l * counts.get(l, 0)
+
+    print(similarity)
+
+
+if __name__ == "__main__":
+    main()
